d21ba3bd234aa46f43435a0ba779a570ed0a81fe
Rudy Alvarez <rudygt@gmail.com>
2021-02-14 17:12:34 +0100

    allow infura or alchemy for multicall client

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,14 @@ set -e
 NETWORK="mainnet" # default to Mainnet (Infura)
 EXPLORER="https://api.etherscan.io/api"
 
+export WEB3_PROVIDER_URI=https://mainnet.infura.io/v3/$WEB3_INFURA_PROJECT_ID
+
 if [[ ! -z "${ALCHEMY_URL}" ]]; then
   if [[ ! $(brownie networks list | grep mainnet-alchemy) ]]; then
     brownie networks add Ethereum mainnet-alchemy host=$ALCHEMY_URL chainid=1 explorer=$EXPLORER
   fi
   NETWORK="mainnet-alchemy"
+  export WEB3_PROVIDER_URI=$ALCHEMY_URL
 fi
 
 if [[ -z $1 ]]; then
